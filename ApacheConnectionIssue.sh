09b405233f93567889c690deb29de6754596b8e0
Luke Shirnia <lukeshirnia@btinternet.com>
2015-07-24 07:09:52 +0100

    Update ApacheConnectionIssue.sh

--- ApacheConnectionIssue.sh
+++ ApacheConnectionIssue.sh
@@ -1,95 +1,133 @@
 #!/bin/bash
- 
+
 ###################################
- 
-neat="################################"
-methodhttpd1=$( cat /etc/*release | grep -ic centos )
-methodapache1=$( cat /etc/*release | grep -ic ubuntu )
- 
-##################################
-#########CENTOS/RHL###############
-apacherunning=$(service httpd status | grep -ic running)
-apacheportcentos=$(netstat -plnt | grep http | awk '{print $4}' | sed 's/://g')
-currentconcentos=$(ps afx | grep -ic /usr/sbin/httpd)
- 
+#####Colours######
+ESC_SEQ="\x1b["
+GREEN=$ESC_SEQ"32;01m"
+RED=$ESC_SEQ"31;01m"
+RESET=$ESC_SEQ"39;49;00m"
+BLUE=$ESC_SEQ"34;01m"
+#####Underline/bold#####
+BOLD=$ESC_SEQ"\033[1m"
+bold=$(tput bold)
+UNDERLINE=$ESC_SEQ"\033[4m"
+#####################################
+
 maxclientscentos=$(grep MaxClients /etc/httpd/conf/httpd.conf | grep processes -A 1 | awk '{print $2}' | grep -v MaxClients)
  
-#####Error Log data#####
-# SORT OUT REGEX FOR ERROR LOGS error.log / error_log
-errorlogformat=$(grep ^ErrorLog /etc/httpd/conf/httpd.conf | awk '{print $2}' | sed 's/.*[/]//')
-errorlogcentos=$( grep -ic maxc /var/log/httpd/$errorlogformat )
-zerrorlogcentos=$( zgrep -i maxc /var/log/httpd/error_log* )
- 
-#####APACHE BUDDY DATA#####
-apachebuddy=$(curl -s apache2buddy.pl | perl | grep -ohe 'Percentage of RAM allocated to Apache.*' | awk '{print $7}')
- 
-apachebuddyram=$(curl -s apache2buddy.pl | perl | grep -ohe 'Max potential memory usage:.*' | awk '{print $5}')
- 
 ##################################
-########Ubuntu/Debian#############
- 
- 
- 
- 
- 
-##################################
- 
-echo $neat
-echo ""
-##################################
-method1() {
- 
-        case $apacherunning in
- 
-#------------------------
-# apache not running!
-        0 )
-            echo "Not running"
-            echo "Please troubleshoot further"
-            echo ""
-            echo $neat
-# error logs?
-#------------------------
-#if apache is running then:
+
+neat="################################"
+DIST=$(cat /etc/issue | head -1 | cut -d' ' -f1)
+
+printf "$neat\n"
+printf "\n"
+
+####################################
+check_distro() {
+Distro=$(cat /etc/issue | head -1 | cut -d' ' -f1)
+if [ $Distro == "CentOS" ] || [ $Distro == "Red Hat" ]; then
+        case $Distro in
+        "CentOS" )
+                Version=$(cat /etc/redhat-release | grep -Eo '[0-9]{1,4}' | head -1)
         ;;
-        1 )
-echo "Apache: is running!"
-echo "Port: "$apacheportcentos
-echo "Current Connections "$currentconcentos
-echo "Configured Max Connections: "$maxclientscentos
+        "Red Hat" )
+                Version=$(cat /etc/redhat-release | grep -Eo '[0-9]{1,4}' | head -1)
+        ;;
+        esac
+elif [ $Distro == "Ubuntu"] || [ $Distro == "Debian" ]; then
+        case $Distro in
+        "Ubuntu" )
+                Version=$(cat /etc/issue | head -1 | cut -d' ' -f2 | cut -d'.' -f1)
+        ;;
+        "Debian" )
+                Version=$( cat /etc/issue | head -1 | cut -d' ' -f3 )
+        ;;
+        esac
+fi
+}
+check_httpd() {
+	httpdrunning=$(/etc/init.d/httpd status | grep -ic 'is running')
+	httpdport=$(netstat -plnt | grep http | awk '{print $4}' | sed 's/://g')
+	if [ ! $httpdport == "" ]; then 
+		printf "Apache Port: $httpdport \n"
+	else
+		printf "Apache Port: No port, Apache not running"
+	fi
+}
+apache_buddy() {
+	curl -s apache2buddy.pl | perl > /dev/null 2>&1; #run apache buddy and redirect output to /dev/null, we are only looking for the log files
+	ab=$(grep -ohe 'Highest Pct .*' /var/log/apache2buddy.log | awk 'END{print $5}' | sed 's/"//g') #getting the ram % allocation for apache from logs produced above
+	abram=$(grep -ohe 'Memory: .*' /var/log/apache2buddy.log | awk 'END{print $2}' | sed 's/"//g')
+	currentconcentos=$(ps aux | grep -v grep | grep -ic /usr/sbin/httpd)
+	MaxcRecommend=$(grep -ohe 'Reccommended: .*' /var/log/apache2buddy.log | awk 'END{print $2}' | sed 's/"//g')
+	MaxcConfigured=$(echo - | awk -v max=$MaxcRecommend -v current=$currentconcentos '{print max - current }')
+}
+httpd_error_logs() {
+    errorlogformat=$(grep ^ErrorLog /etc/httpd/conf/httpd.conf | awk '{print $2}' | sed 's/.*[/]//')
+    errorlogcentos=$( grep -ic maxc /var/log/httpd/$errorlogformat )
+    zerrorlogcentos=$( zgrep -i maxc /var/log/httpd/error_log* )
+}
+httpd_calculations() {
+httpd_error_logs
+difference=$(echo - | awk -v apachebuddy=$MaxcRecommend -v current=$maxclientscentos '{print current - apachebuddy}') #compare 
+
+    case $difference in
+    0)
+        printf "Recommended connections: $MaxcRecommend\n"
+        printf "Configured Max Connections: "$maxclientscentos
+    ;;
+   
+    *)
+        printf "Connections is configured correctly!!\n"
+      ;;
+    esac
+   
+    case $MaxcConfigured in
+    1 ) #change to > 1 because it could be less ####################################################################################################################################
+        echo "Reached max connections!!: "$MaxcConfigured
+        echo "Warning, config crap"
+    ;;
+    *)
+        echo "Your Current Conenctions: "$currentconcentos
+        echo "Difference = "$MaxcConfigured
+    ;;    
+    esac
+
+
 #if there is a value configured in apache config file for MaxClients then calculate difference between current connections and configured connections
-    if [ "$maxclientscentos" != "" ] && [ "$maxclientscentos" -ge 1 ];then
-        maxcdiff=$(awk '{$maxclientscentos - $currentconcentos}')
-        echo "Difference = "$maxcdiff
-            if [ "$maxcdiff" -ge 1 ]; then
+if [ "$maxclientscentos" != "" ] && [ "$maxclientscentos" -ge 1 ];then
+       # maxcdiff=$(echo - | awk -v mc=$maxclientsrecommended -v cc=$currentconcentos '{print mc - cc}')
+#    echo "Current Connections "$currentconcentos
+
+            if [ "$MaxcConfigured" -ge 1 ]; then
                 echo "MaxClients: Not been reached"
-            elif [ "$maxcdiff" = [1-9] ]; then
+            elif [ "$MaxcConfigured" = [1-9] ]; then
                 echo "MaxClients: CLOSE TO LIMIT"
             else
                 echo "MaxClients: LIMIT REACHED!"
             fi
 # if no value has been added then
-    else
+else
         echo ""
         echo "MaxClients: No Configured Value In Apache Config File!!"
         echo "Checking with apache buddy..."
-    fi
-   
- #add awk for maxcdiff=$(awk '{$maxclientscentos - $currentconcentos}')
+fi
+    #add awk for maxcdiff=$(awk '{$maxclientscentos - $currentconcentos}')
+
  
- 
-    if [ "$errorlogcentos" -ge 1 ]; then
+if [ "$errorlogcentos" -ge 1 ]; then
 # maxclients may have been hit a previous day, try to incoporate date in the search
         echo "Error logs:"
         echo $errorlogscentos
-    else #elif
+else #elif
 ####APACHE BUDDY SECTION####    
         echo ""
-        echo "Error Logs: Nothing to report!"
-        echo "Current RAM allocation to apache: $apachebuddy%"
-        echo "Apache Max RAM Usage: $apachebuddyram MB"
+        echo "Error Logs: Nothing regarding MaxClients"
+        echo "Current RAM allocation to apache: $ab%"
+        echo "Apache Max RAM Usage: $abram MB"
  
-        case $apachebuddy in
+        case $ab in
         [0-75] )
             echo "apache has been allocated too much ram, this could be causing an issue"
         ;;
@@ -98,63 +136,58 @@ echo "Configured Max Connections: "$maxclientscentos
         ;;
         esac
 #############################
-echo ""
-echo $neat
-#else
-    fi
- 
- 
- 
- 
-echo $neat
- 
- 
-;;
+    echo ""
+    echo $neat
+fi
+echo $neat    
+}
+##################################
+method1() {
+case $httpdrunning in
+ #-----------------------
+        0 ) #if apache is not running:
+            printf "Not running\n"
+            printf "Please troubleshoot further\n"
+            printf "\n"
+            printf "$neat\n"
+#------------------------
+        ;;
+        1 ) #if apache IS running:
+            printf "Apache: is running!\n"
+            apache_buddy
+            printf "\n"
+            httpd_calculations
+        ;;
         esac
 }
- 
- 
-#################################################
-#Ubuntu
-method2() {
- 
- 
- 
-echo "test"
- 
- 
- 
- 
-}
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
 ##################################
-if [ $methodhttpd1 -ge 1 ]; then
- 
- 
+########Start of code#############
+##################################
+check_distro
+
+if [ "$Distro" == "CentOS" ] && [ "$Version" -lt 7 ] || [ "$Distro" == "Red Hat" ] && [ "$Version" -lt 7 ]; then
+
+check_httpd
 method1
  
- 
-##################################
-#elif [ $methodapache1 -ge 1 ]; then
- 
- 
-#method2
- 
- 
-###################################
+
+elif [ "$Distro" == "Ubuntu" ] && [ "$Version" -gt 12] && [ $Version -lt 14 ]; then
+
+
+printf "Ubuntu\n"
+
+
+elif [ "$Distro" = "Debian" ] && [ "$Version" = 7 ]; then
+
+
+
+printf "Debian Not Supported Yet\n"
+
+
+
 else
- 
-echo "Error! Server does not appear to be Ubuntu or Centos"
+
+echo "Error! Server does not appear to be a supported version of Ubuntu or Centos"
  
 fi
 ###################################
